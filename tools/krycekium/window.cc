4f91331654ddb3ef2f7e737d1dc85124431b341e
Force Charlie <charlieio@outlook.com>
2019-10-15 20:10:28 +0800

    [krycekium] message

--- tools/krycekium/window.cc
+++ tools/krycekium/window.cc
@@ -1,9 +1,45 @@
 ///
 #include "window.hpp"
+// PE BaseAddress
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 
 namespace krycekium {
+Window::Window() {
+  //
+  hInst = reinterpret_cast<HINSTANCE>(&__ImageBase);
+}
+
+Window::~Window() {
+  //
+}
+
 bool Window::WindowInitialize() {
   //
   return true;
 }
+
+LRESULT Window::OnCreate(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                         BOOL &bHandle) {
+  return S_OK;
+}
+LRESULT Window::OnDestroy(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                          BOOL &bHandle) {
+  return S_OK;
+}
+LRESULT Window::OnClose(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                        BOOL &bHandle) {
+  return S_OK;
+}
+
+// User defined message handler
+LRESULT Window::OnExecutorNotify(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                                 BOOL &bHandle) {
+  return S_OK;
+}
+
+LRESULT Window::OnExecutorProgress(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                                   BOOL &bHandle) {
+  return S_OK;
+}
+
 } // namespace krycekium
--- tools/krycekium/window.hpp
+++ tools/krycekium/window.hpp
@@ -26,20 +26,31 @@ public:
 protected:
   DECLARE_WND_CLASS(WindowName)
   BEGIN_MSG_MAP(Window)
-  //   MESSAGE_HANDLER(WM_CREATE, OnCreate)
-  //   MESSAGE_HANDLER(WM_CLOSE, OnClose)
-  //   MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+  MESSAGE_HANDLER(WM_CREATE, OnCreate)
+  MESSAGE_HANDLER(WM_CLOSE, OnClose)
+  MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
   //   MESSAGE_HANDLER(WM_SIZE, OnSize)
   //   MESSAGE_HANDLER(WM_PAINT, OnPaint)
   //   MESSAGE_HANDLER(WM_DPICHANGED, OnDpiChanged)
   //   MESSAGE_HANDLER(WM_DISPLAYCHANGE, OnDisplayChange)
   //   MESSAGE_HANDLER(WM_DROPFILES, OnDropfiles)
+  MESSAGE_HANDLER(WM_EXECUTOR_NOTIFY, OnExecutorNotify)
+  MESSAGE_HANDLER(WM_EXECUTOR_PROGRESS, OnExecutorProgress)
   //   SYSCOMMAND_ID_HANDLER(IDM_KRYCEKIUM_ABOUT, OnKrycekiumAbout)
   //   COMMAND_ID_HANDLER(IDC_PACKAGE_VIEW_BUTTON, OnDiscoverPackage)
   //   COMMAND_ID_HANDLER(IDC_FOLDER_URI_BUTTON, OnDiscoverFolder)
   //   COMMAND_ID_HANDLER(IDC_OPTION_BUTTON_OK, OnStartTask)
   //   COMMAND_ID_HANDLER(IDC_OPTION_BUTTON_CANCEL, OnCancelTask)
   END_MSG_MAP()
+  LRESULT OnCreate(UINT nMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandle);
+  LRESULT OnDestroy(UINT nMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandle);
+  LRESULT OnClose(UINT nMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandle);
+  LRESULT OnExecutorNotify(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                           BOOL &bHandle);
+  LRESULT OnExecutorProgress(UINT nMsg, WPARAM wParam, LPARAM lParam,
+                             BOOL &bHandle);
+
 private:
+  HINSTANCE hInst;
 };
 } // namespace krycekium
