3d08b99183ae399937073496bc272d89cdd98f7f
Force Charlie <charlieio@outlook.com>
2019-10-17 20:42:11 +0800

    update progressbar

--- tools/kisasum/progressbar.hpp
+++ tools/kisasum/progressbar.hpp
@@ -13,10 +13,19 @@ public:
   void Initialize(int64_t to) {
     total = to;
     completed = 0;
-    start_time = std::chrono::steady_clock::now();
+    tick = std::chrono::steady_clock::now();
+    start_time = tick;
   }
   void Update(int64_t bytes) {
-    //
+    auto now = std::chrono::steady_clock::now();
+    completed += bytes;
+    if (std::chrono::duration_cast<std::chrono::milliseconds>(now - tick)
+            .count() > 10) {
+      auto diff = std::chrono::duration_cast<std::chrono::nanoseconds>(
+          tick - start_time);
+      auto speed = completed * 1000'000'000 / diff.count();
+    }
+    tick = now;
   }
   void Refresh() {
     //
@@ -29,6 +38,7 @@ private:
   int64_t total{0};
   int64_t completed{0};
   std::chrono::steady_clock::time_point start_time;
+  std::chrono::steady_clock::time_point tick;
 };
 } // namespace kisasum
 
