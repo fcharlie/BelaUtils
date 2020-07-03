718d5cca64b59d16f031344a4d20ad48c8a26d7b
Force.Charlie <charlieio@outlook.com>
2020-07-04 04:00:33 +0800

    belautils 1.1

--- tools/kisasum/cli/kisasum.cc
+++ tools/kisasum/cli/kisasum.cc
@@ -25,11 +25,15 @@ OPTIONS:
 Algorithm:
   SHA224     SHA256     SHA384     SHA512
   SHA3-224   SHA3-256   SHA3-384   SHA3-512
-  BLAKE3     BLAKE2s    BLAKE2b    KangarooTwelve
+  BLAKE3     BLAKE2s    BLAKE2b    KangarooTwelve*
 
 Formats:
   text     format to text, support progress
   json     format to json
+
+Notes:
+  KangarooTwelve Experimental Support
+
 )";
   bela::FPrintF(stderr, L"%s\n", ua);
 }
