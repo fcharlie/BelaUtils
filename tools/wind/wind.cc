09e28db207b92208654b4b40a86c53dd5cebb60e
Force.Charlie <charlieio@outlook.com>
2020-07-04 20:22:28 +0800

    cleanup bela warning

---
+++ tools/wind/wind.cc
@@ -0,0 +1,13 @@
+// A simple program download network resource
+#include "wind.hpp"
+namespace baulk {
+bool IsDebugMode = false;
+bool IsForceMode = false;
+wchar_t UserAgent[UerAgentMaximumLength] = L"Wget/5.0 (Baulk)";
+} // namespace baulk
+
+int wmain(int argc, wchar_t **argv) {
+  //
+
+  return 0;
+}
