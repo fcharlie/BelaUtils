7d45bf73d532a4bbf78b8db3b4459eefd6ca709f
Force Charlie <charlieio@outlook.com>
2020-11-19 21:50:30 +0800

    Use New bela

--- tools/caelum/pecoff.cc
+++ tools/caelum/pecoff.cc
@@ -29,37 +29,54 @@ inline std::wstring flatvector(const std::vector<std::wstring> &v, std::wstring_
 bool Window::InquisitivePE() {
   auto path = wUrl.Content();
   bela::error_code ec;
-  auto pea = bela::pe::Expose(path, ec);
-  if (!pea) {
+  auto file = bela::pe::NewFile(path, ec);
+  if (!file) {
     bela::BelaMessageBox(hWnd, L"Inquisitive PE ", ec.message.data(), nullptr, bela::mbs_t::FATAL);
     return false;
   }
-  auto vi = bela::pe::ExposeVersion(path, ec);
+  bela::pe::FunctionTable ft;
+  if (!file->LookupFunctionTable(ft, ec)) {
+    bela::BelaMessageBox(hWnd, L"Inquisitive PE ", ec.message.data(), nullptr, bela::mbs_t::FATAL);
+    return false;
+  }
+  auto vi = bela::pe::LookupVersion(path, ec);
   if (vi && !vi->ProductName.empty()) {
     tables.Append(L"ProductName:", vi->ProductName);
   }
-  tables.Append(L"Machine:", caelum::Machine(static_cast<uint32_t>(pea->machine)));
-  tables.Append(L"Subsystem:", caelum::Subsystem(static_cast<uint32_t>(pea->subsystem)));
-  tables.Append(L"OS Version:", pea->osver.Str());
-  tables.Append(L"Link Version:", pea->linkver.Str());
-  if (!pea->clrmsg.empty()) {
-    tables.Append(L"CLR Details:", pea->clrmsg);
+  tables.Append(L"Machine:", caelum::Machine(static_cast<uint32_t>(file->Machine())));
+  tables.Append(L"Subsystem:", caelum::Subsystem(static_cast<uint32_t>(file->Subsystem())));
+
+  uint16_t dllcharacteristics{0};
+  if (file->Is64Bit()) {
+    auto oh = file->Oh64();
+    dllcharacteristics = oh->DllCharacteristics;
+    tables.Append(L"OS Version:", bela::StringCat(oh->MajorOperatingSystemVersion, oh->MinorOperatingSystemVersion));
+    tables.Append(L"Link Version:", bela::StringCat(oh->MajorLinkerVersion, oh->MajorLinkerVersion));
+  } else {
+    auto oh = file->Oh64();
+    dllcharacteristics = oh->DllCharacteristics;
+    tables.Append(L"OS Version:", bela::StringCat(oh->MajorOperatingSystemVersion, oh->MinorOperatingSystemVersion));
+    tables.Append(L"Link Version:", bela::StringCat(oh->MajorLinkerVersion, oh->MajorLinkerVersion));
   }
+
+  // if (!pea->clrmsg.empty()) {
+  //   tables.Append(L"CLR Details:", pea->clrmsg);
+  // }
 
   tables.Append(L"Characteristics:");
   tables.Append(L"Depends:");
 
   auto y = 80 + 30 * tables.ats.size();
-  auto charsv = caelum::Characteristics(pea->characteristics, pea->dllcharacteristics);
+  auto charsv = caelum::Characteristics(file->Fh().Characteristics, dllcharacteristics);
   // depends lab append
-
-  auto strcharsv = flatvector(charsv);
-  auto depends = flatvector(pea->depends, L"\r\n");
-  if (!pea->delays.empty()) {
-    auto delaydepends = flatvector(pea->delays, L"\r\n");
-    bela::StrAppend(&depends, L"**below is delay**\r\n", delaydepends);
+  std::wstring depends;
+  for (auto &im : ft.imports) {
+    bela::StrAppend(&depends, bela::ToWide(im.first), L" (", im.second.size(), L")\r\n");
   }
-
+  for (auto &im : ft.delayimprots) {
+    bela::StrAppend(&depends, L"(Delay) ", bela::ToWide(im.first), L" (", im.second.size(), L")\r\n");
+  }
+  auto strcharsv = flatvector(charsv);
   constexpr auto es =
       WS_CHILDWINDOW | WS_VISIBLE | WS_TABSTOP | WS_VSCROLL | ES_LEFT | ES_AUTOVSCROLL | ES_MULTILINE | ES_READONLY;
   constexpr auto exs = WS_EX_LEFT | WS_EX_LTRREADING | WS_EX_RIGHTSCROLLBAR | WS_EX_NOPARENTNOTIFY;
